33b4e65d33852e29b98395e9313c14d94b6282ac
Jaden-Reklaw <jwalker117@gmail.com>
2024-04-15 08:03:51 -0500

    completed cdk command exercise

---
+++ cdk.commands.md
@@ -0,0 +1,25 @@
+# CDK Commnads
+
+### cdk init app --language typescript
+== create project == 
+
+### cdk bootstrap
+== sets up env, deploys bootstrap stack, provides configuration, check aws permissions ==
+
+### cdk synth
+
+
+### cdk deploy
+
+### cdk list
+== list the stacks in the code ==
+
+### cdk diff
+== checks differences between what is deployed in aws and the code ==
+
+
+### cdk doctor
+== checks for issues in cdk, rarely used ==
+
+### cdk destory [nameOfStack]
+== deletes the stack in aws ==
--- lib/demo-stack.ts
+++ lib/demo-stack.ts
@@ -1,16 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Duration } from 'aws-cdk-lib';
 
+//L3 Resource
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id)
+
+    new Bucket(this, 'MyL3Bucket', {
+      lifecycleRules: [{
+        expiration: Duration.days(expiration)
+      }]
+    });
+  }
+}
 export class DemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //create an s3 bucket 3 ways
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //L1 Resource
+    new CfnBucket(this, 'MyL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [{
+          expirationInDays: 1,
+          status: 'Enabled'
+        }]
+      }
+    });
+
+    //L2 Resource
+    new Bucket(this, 'MyL2Bucket', {
+      lifecycleRules: [{
+        expiration: Duration.days(2)
+      }]
+    });
+
+    new L3Bucket(this, 'MyL3Bucket', 3)
   }
 }
