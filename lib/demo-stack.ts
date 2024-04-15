a2019575ad73c924520b8010e08de23e707b141f
Jaden-Reklaw <jwalker117@gmail.com>
2024-04-15 08:09:42 -0500

    completed cdk print output using cdk

--- lib/demo-stack.ts
+++ lib/demo-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import { Duration } from 'aws-cdk-lib';
+import { CfnOutput } from 'aws-cdk-lib';
 
 //L3 Resource
 class L3Bucket extends Construct {
@@ -32,11 +33,17 @@ export class DemoStack extends cdk.Stack {
     });
 
     //L2 Resource
-    new Bucket(this, 'MyL2Bucket', {
+    const myL2Bucket = new Bucket(this, 'MyL2Bucket', {
       lifecycleRules: [{
         expiration: Duration.days(2)
       }]
     });
+
+    //how to send output info to cloudformation using cdk
+    //this will list the name of the bucket in cloudformation in the output tab
+    new CfnOutput(this, 'MyL2BucketName', {
+      value: myL2Bucket.bucketName
+    })
 
     new L3Bucket(this, 'MyL3Bucket', 3)
   }
