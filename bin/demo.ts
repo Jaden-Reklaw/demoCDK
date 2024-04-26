880027df83ad2e2ea85e06e379af0e66c385b66d
Jaden-Reklaw <jwalker117@gmail.com>
2024-04-26 09:55:42 -0500

    a way to add tags

--- bin/demo.ts
+++ bin/demo.ts
@@ -4,9 +4,13 @@ import * as cdk from 'aws-cdk-lib';
 import { DemoStack } from '../lib/demo-stack';
 import { PhotosStack } from '../lib/photo-stack';
 import { PhotosHandlerStack } from '../lib/photo-handler-stack';
+import { BucketTagger } from './tags';
 
 const app = new cdk.App();
 const photosStack = new PhotosStack(app, 'PhotosStack');
 const photosHandlerStack = new PhotosHandlerStack(app, 'PhotosHandlerStack', {
     targetBucketArn: photosStack.photosBucketArn
-})
+});
+
+const tagger = new BucketTagger('level', 'test');
+cdk.Aspects.of(app).add(tagger);
